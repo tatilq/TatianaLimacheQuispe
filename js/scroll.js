8f772a97601cb79ae1dca530e4948d3d4a1a5bfb
Tatiana Limache Quispe <tati34che@gmail.com>
2017-04-08 03:30:08 -0500

    Añadi menu scroll

---
+++ js/scroll.js
@@ -0,0 +1,11 @@
+$('.post_article').waypoint(
+function(direction) {
+if (direction ==='down') {
+var wayID = $(this).attr('id');
+} else {
+var previous = $(this).prev();
+var wayID = $(previous).attr('id');
+}
+$('.current').removeClass('current');
+$('#main_nav a[href=#'+wayID+']').addClass('current');
+}, { offset: '40%' });
